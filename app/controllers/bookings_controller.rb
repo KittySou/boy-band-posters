ce7e7bdbea7344afb37ac77bc1f94d499d365dba
davidricardo <david.ricardo.1991@gmail.com>
2023-08-22 17:29:14 -0400

    change

---
+++ app/controllers/bookings_controller.rb
@@ -0,0 +1,22 @@
+class BookingsController < ApplicationController
+  def index
+    @bookings_user = Booking.where(user: current_user)
+
+
+
+
+  end
+
+  def new
+    @booking = Booking.new
+  end
+
+  def create
+    @booking = Booking.new(booking_params)
+    if @booking.save
+      redirect_to booking_path(@booking)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+end
