9220d5f7b57c8628ccc1b02eb27e8aea800d020d
Marcelo Alves <marceloaa@live.com>
2023-08-23 15:21:53 -0400

    booking-new in progress...

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -6,6 +6,7 @@ class BookingsController < ApplicationController
   def new
     @booking = Booking.new
   end
+
   def create
     @booking = Booking.new(booking _params)
     @booking.poster = @poster
@@ -14,27 +15,31 @@ class BookingsController < ApplicationController
     else
       render :new, status: :unprocessable_entity
     end
+
   def show
     set_booking
     @poster = @booking.poster
   end
+
   def update
     set_booking
     @booking.status = "Pending "
     @booking.save!
     redirect_to booking_path(@booking)
   end
+
   def destroy
     @booking = Booking.find(params[:id])
     @booking.destroy
     redirect_to booking(@review.poster), status: :see_other
   end
+
   private
 def booking_params
   params.require(:booking).permit(:checkin_on, :checkout_on, :value, :status)
 end
-def set_booking
-  @booking = Booking.find(params[:id])
-end
+
+  def set_booking
+    @booking = Booking.find(params[:id])
   end
 end
