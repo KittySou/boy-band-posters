ec1b5c0cb09fbdcf73f2f1c1076451c02c0ecd9c
Marcelo Alves <marceloaa@live.com>
2023-08-23 14:56:03 -0400

    home page completed

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,18 +1,40 @@
 class BookingsController < ApplicationController
-  def index
-    @bookings_user = Booking.where(user: current_user)
-  end
 
+  def index
+    @bookings = Booking.where(user_id: current_user.id)
+  end
   def new
     @booking = Booking.new
   end
-
   def create
-    @booking = Booking.new(booking_params)
-    if @booking.save
-      redirect_to booking_path(@booking)
+    @booking = Booking.new(booking _params)
+    @booking.poster = @poster
+    if @review.save
+      redirect_to poster_path(@poster)
     else
       render :new, status: :unprocessable_entity
     end
+  def show
+    set_booking
+    @poster = @booking.poster
+  end
+  def update
+    set_booking
+    @booking.status = "Pending "
+    @booking.save!
+    redirect_to booking_path(@booking)
+  end
+  def destroy
+    @booking = Booking.find(params[:id])
+    @booking.destroy
+    redirect_to booking(@review.poster), status: :see_other
+  end
+  private
+def booking_params
+  params.require(:booking).permit(:checkin_on, :checkout_on, :value, :status)
+end
+def set_booking
+  @booking = Booking.find(params[:id])
+end
   end
 end
