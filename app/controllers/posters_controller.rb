769ed09beb46ac40091edb3c4fb3103e6490c986
davidricardo <david.ricardo.1991@gmail.com>
2023-08-24 17:49:52 -0400

    description

--- app/controllers/posters_controller.rb
+++ app/controllers/posters_controller.rb
@@ -1,6 +1,9 @@
 class PostersController < ApplicationController
   def index
     @posters = Poster.all
+      if params[:query].present?
+      @poster = Poster.global_search(params[:query])
+      end
   end
 
   def new
--- app/models/poster.rb
+++ app/models/poster.rb
@@ -1,13 +1,24 @@
 class Poster < ApplicationRecord
-  include PgSearch::Model
   belongs_to :user
   has_one_attached :photo
   has_many :bookings, dependent: :destroy
   validates :photo, presence: true
 
+  include PgSearch::Model
+
+
   pg_search_scope :search_by_band_name_and_description,
-  against: [ :band_name, :description ],
-  using: {
-    tsearch: { prefix: true }
-  }
+                  against: [:band_name, :description],
+                  using: {
+                    tsearch: { prefix: true }
+                  }
+
+                  pg_search_scope :global_search,
+                  against: [:band_name, :description],
+                  associated_against: {
+                    user: [:band_name, :description]
+                  },
+                  using: {
+                    tsearch: { prefix: true }
+                  }
 end
