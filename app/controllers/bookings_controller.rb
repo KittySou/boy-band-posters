f2d0ea8480792f8f19edab14a63722e1a19a3c25
Kitty Souriyadeth <kitty.souriyadeth@hotmail.fr>
2023-08-25 08:00:15 -0400

    some changes

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,5 +1,4 @@
 class BookingsController < ApplicationController
-
   def index
     @bookings_user = Booking.where(user: current_user)
   end
@@ -9,49 +8,51 @@ class BookingsController < ApplicationController
   end
 
   def create
-    @booking = Booking.new(booking _params)
-    @booking.poster = @poster
-    if @review.save
+    @booking = Booking.new(booking_params)
+    @poster = Poster.find(params[:id])
+
+    if @booking.save
       redirect_to poster_path(@poster)
     else
       render :new, status: :unprocessable_entity
     end
   end
 
-    def destroy
-      @booking = Booking.find(params[:id])
-      @booking.destroy
-      redirect_to bookings_path, status: :see_other
-    end
+  def destroy
+    @booking = Booking.find(params[:id])
+    @booking.destroy
+    redirect_to bookings_path, status: :see_other
+  end
 
- # If statement for status, ALWAYS TRUE OR FALSE
   def update
-
+    # If statement for status, ALWAYS TRUE OR FALSE
     set_booking
     @booking.status = true
     @booking.save!
     redirect_to booking_path(@booking)
   end
 
-    def index
-      @bookings = Booking.all
-    end
-
-    def show
-      @booking = Booking.find(params[:id])
-    end
-
-    def new
-      @booking = Booking.new
-    end
-
-    private
-
-    def booking_params
-      params.require(:booking).permit(:checkin_on, :checkout_on, :value, :status)
-    end
-
-    def set_booking
-      @booking = Booking.find(params[:id])
-    end
+  def index
+    @bookings = Booking.all
   end
+
+  def show
+    @booking = Booking.find(params[:id])
+  end
+
+  def total_price
+    @poster = Poster.find(params[:id])
+    @total_days = (@booking.checkout_on - @booking.checkin_on).to_i
+    @total_price = total_days * @booking.price_per_day
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:checkin_on, :checkout_on, :value, :status)
+  end
+
+  def set_booking
+    @booking = Booking.find(params[:id])
+  end
+end
